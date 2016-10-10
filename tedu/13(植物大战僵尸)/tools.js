b16750650ed524a456c35667801fd823b0308046
wsk <weishuaikun@163.com>
2016-10-10 16:59:54 +0800

    init

---
+++ tedu/13(植物大战僵尸)/tools.js
@@ -0,0 +1,96 @@
+//加载背景图片
+var bg = new Image();
+bg.src = "img/bg_play.jpg";
+//加载僵尸吃了脑子图片
+var bg_end = new Image();
+bg_end.src = "img/ZombiesWon.png";
+
+//加载僵尸移动图片
+var zombieM1 = new Image();
+zombieM1.src = "img/move/01.png";
+var zombieM2 = new Image();
+zombieM2.src = "img/move/02.png";
+var zombieM3 = new Image();
+zombieM3.src = "img/move/03.png";
+var zombieM4 = new Image();
+zombieM4.src = "img/move/04.png";
+var zombieM5 = new Image();
+zombieM5.src = "img/move/05.png";
+var zombieM6 = new Image();
+zombieM6.src = "img/move/06.png";
+var zombieM7 = new Image();
+zombieM7.src = "img/move/07.png";
+var zombieM8 = new Image();
+zombieM8.src = "img/move/08.png";
+var zombieM9 = new Image();
+zombieM9.src = "img/move/09.png";
+var zombieM10 = new Image();
+zombieM10.src = "img/move/10.png";
+var zombieM11 = new Image();
+zombieM11.src = "img/move/11.png";
+var zombieM12 = new Image();
+zombieM12.src = "img/move/12.png";
+var zombieM13 = new Image();
+zombieM13.src = "img/move/13.png";
+//加载僵尸站立的图片
+var zombieS1 = new Image();
+zombieS1.src = "img/stand/21.png";
+var zombieS2 = new Image();
+zombieS2.src = "img/stand/22.png";
+var zombieS3 = new Image();
+zombieS3.src = "img/stand/23.png";
+var zombieS4 = new Image();
+zombieS4.src = "img/stand/24.png";
+var zombieS5 = new Image();
+zombieS5.src = "img/stand/25.png";
+var zombieS6 = new Image();
+zombieS6.src = "img/stand/26.png";
+//加载僵尸攻击图片
+var zombieA1 = new Image();
+zombieA1.src = "img/attack/31.png";
+var zombieA2 = new Image();
+zombieA2.src = "img/attack/32.png";
+var zombieA3 = new Image();
+zombieA3.src = "img/attack/33.png";
+var zombieA4 = new Image();
+zombieA4.src = "img/attack/34.png";
+var zombieA5 = new Image();
+zombieA5.src = "img/attack/35.png";
+var zombieA6 = new Image();
+zombieA6.src = "img/attack/36.png";
+var zombieA7 = new Image();
+zombieA7.src = "img/attack/37.png";
+var zombieA8 = new Image();
+zombieA8.src = "img/attack/38.png";
+var zombieA9 = new Image();
+zombieA9.src = "img/attack/39.png";
+var zombieA10 = new Image();
+zombieA10.src = "img/attack/40.png";
+var zombieA11 = new Image();
+zombieA11.src = "img/attack/41.png";
+
+//画植物——坚果
+var nut1 = new Image();
+nut1.src = "img/plants/TallNut.gif";
+var nut2 = new Image();
+nut2.src = "img/plants/TallnutCracked1.gif";
+var nut3 = new Image();
+nut3.src = "img/plants/TallnutCracked2.gif";
+//其他植物
+var plant1 = new Image();
+plant1.src = "img/plants/IceShroom.gif";
+var plant2 = new Image();
+plant2.src = "img/plants/HypnoShroom.gif";
+var plant3 = new Image();
+plant3.src = "img/plants/SunFlower.gif";
+var plant4 = new Image();
+plant4.src = "img/plants/WallNut.gif";
+
+
+
+
+
+
+
+
+
